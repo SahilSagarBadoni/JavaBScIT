30a61d3a734570f409c7a5beade972ad01107207
SahilSagarBadoni <85783834+SahilSagarBadoni@users.noreply.github.com>
2021-06-12 16:30:26 +0530

    Add files via upload

---
+++ BScIT/SahilSagarBadoni-1922054/IntegerProgram.java
@@ -0,0 +1,11 @@
+class IntegerProgram
+ {
+  public static void main(String[]args)
+  {
+   System.out.println("First 10 Integer Are\n");
+   for(int i=1;i<=10;i++)
+   {
+    System.out.println(i);
+   }
+  }
+ }
